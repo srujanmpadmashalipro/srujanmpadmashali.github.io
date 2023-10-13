f19757dd219afbd897ab0b1c13e156b1da7b8e93
Srujan M Padmashali <142640643+srujanmpadmashali@users.noreply.github.com>
2023-10-13 22:27:36 +0530

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,8 +1,1 @@
-const expandButtons = document.querySelectorAll('.expand-menu');
 
-for (const button of expandButtons) {
-  button.addEventListener('click', () => {
-    const menuItems = button.nextElementSibling.querySelector('.menu-items');
-    menuItems.classList.toggle('hidden');
-  });
-}
